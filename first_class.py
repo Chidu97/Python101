65986e8149abaf6719893a6ae28e42ef503562ef
Chidu <chidu.ugo@hotmail.com>
2022-01-18 15:51:55 +0100

    Variables, values, expression, operators

---
+++ first_class.py
@@ -0,0 +1,99 @@
+# print("Welcome to pyhton!")
+
+
+# checking types
+
+# print(type("5"))
+# print(type(5))
+# print(type(5.0))
+
+# changing types
+
+# print(type(int("5")))
+# print(type(str(5)))
+
+# my_name = "Chidu Ugochukwu"
+
+# print(my_name)
+
+# a= 5, b= 10, c= 3
+
+# x = (-10 + 60 ** 0.5/10)
+
+# print(x)
+
+# a=5
+# b=10
+# c=3
+
+# answer = (-b + 4*a*c**0.5/2*a)
+
+# print(answer)
+
+# a = 1/2
+
+# print(a)
+
+# r = 5
+
+# r= 4/3*3.14*5**3
+
+# print(r)
+
+# easypace = 492 * 2
+# tempo = 432 * 3
+
+# time = easypace + tempo
+
+# print(time)
+
+# mins=2280/60
+
+# print(mins)
+
+
+price_for_60 = (24.95 * 60) * 0.6
+
+shipping = 3 + (0.75 * 59)
+print(price_for_60+shipping)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
